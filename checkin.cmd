e8033bd786a70b762880f8c959f8d53508db3bff
spivegin <spivegin@txtsme.com>
2020-02-12 12:28:39 -0500

    Create a short url end point to route traffic to other sites fe18789..0165da6

--- checkin.cmd
+++ checkin.cmd
@@ -1,3 +1,3 @@
 git add -A
-git commit -m "Create a short url end point to route traffic to other sites e9c3fbb..fe18789"
+git commit -m "Create a short url end point to route traffic to other sites fe18789..0165da6 "
 git push
